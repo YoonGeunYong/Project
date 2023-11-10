7be02def2031158447632c75a370a0e12ead4e25
taesoo-jeong <jp430612@naver.com>
2023-11-10 20:36:07 +0900

    231110 update
    카트 애니메이션 수정
    카트 애니메이션 CreateMap 씬에 적용
    카트 애니메이션 움직임이 조금 부자연스러움. 감각이 살아있는 사람이 수정해야할 거 같음;;
    PlayerMovement 스크립트 수정
    박스 이동 스크립트 수정
    사다리 스크립트 수정

--- Project_J_Test/Assets/_Scripts/Player/PlayerMovement2.cs
+++ Project_J_Test/Assets/_Scripts/Player/PlayerMovement2.cs
@@ -14,6 +14,7 @@ public class PlayerMovement2 : MonoBehaviour
     FixedJoint2D fixJoint;
     Animator anim;
     GameObject box;
+    GameObject ladder;
     public ItemManager itemManager;
 
     public bool isRunning;
@@ -107,6 +108,8 @@ public class PlayerMovement2 : MonoBehaviour
             //gravity on the ladder
             //사다리 타기
             case 1 when Input.GetKeyDown(KeyCode.UpArrow):
+                this.transform.position = new Vector3(ladder.transform.position.x,
+                    this.transform.position.y, this.transform.position.z);
                 rb.velocity = Vector3.zero;
                 isLaddering = 2;
                 rb.gravityScale = 0;
@@ -125,16 +128,23 @@ public class PlayerMovement2 : MonoBehaviour
                 rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
                 break;
         }
-        
-        if(isPush && Input.GetKey(KeyCode.F))
+
+        if (isPush)
         {
-            box.gameObject.GetComponent<MoveBox>().isMove = true;
-        }
-        
-        if(isPush && Input.GetKeyUp(KeyCode.F))
-        {
-            box.gameObject.GetComponent<MoveBox>().isMove = false;
-            isPush = false;
+            MoveBox moveBox = box.gameObject.GetComponent<MoveBox>();
+            moveBox.isMove = 1;
+
+            if (Input.GetKey(KeyCode.F))
+            {
+                moveBox.isMove = 2;
+            }
+
+            if (Input.GetKeyUp(KeyCode.F))
+            {
+                moveBox.isMove = 0;
+                moveBox.ZeroVelocity();
+                isPush = false;
+            }
         }
 
 
@@ -185,6 +195,12 @@ public class PlayerMovement2 : MonoBehaviour
         }
     }
 
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (!isPush && collision.gameObject.CompareTag("MoveObj"))
+            isPush = false;
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Ladder"))
@@ -193,6 +209,7 @@ public class PlayerMovement2 : MonoBehaviour
             if (other.GetComponent<ClimbLadder>().isOpen)
             {
                 isLaddering = 1;
+                ladder = other.gameObject;
             }
         }
 
@@ -222,6 +239,7 @@ public class PlayerMovement2 : MonoBehaviour
         {
             isLaddering = 0;
             rb.gravityScale = 3;
+            ladder = null;
         }
 
         if (other.gameObject.CompareTag("pRope"))
